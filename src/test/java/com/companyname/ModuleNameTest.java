fb078753a8f7eead0531f81640ac8b8706b08c26
kamlesh <kamleshkec119@gmail.com>
2020-04-11 09:38:32 +0530

    add new file

---
+++ src/test/java/com/companyname/ModuleNameTest.java
@@ -0,0 +1,14 @@
+package com.companyname;
+
+import org.testng.annotations.Test;
+
+public class ModuleNameTest {
+	@Test
+	
+	public void testCaceNameTest()
+	{
+		System.out.println("++++++First++++=======hi==================");
+		
+	}
+
+}
---
+++ src/test/java/com/companyname/SubsTest.java
@@ -0,0 +1,12 @@
+package com.companyname;
+
+import org.testng.annotations.Test;
+
+public class SubsTest {
+	@Test
+	public void cubstraction()
+	{
+		System.out.println("========substraction done===========");
+	}
+
+}
